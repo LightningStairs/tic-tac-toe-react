a4c654efc972afd17d302e7ca37176ad4fcdd3b2
Jenah <jenah.blitz@simplybusiness.com>
2020-04-01 16:08:20 -0400

    Set state and pass that shit on down as props to the square component

--- src/index.js
+++ src/index.js
@@ -6,16 +6,31 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={ () => { alert('click'); } }>
-        { this.props.value}
+      <button
+        className="square"
+        onClick={() => this.props.onClick() }
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -43,7 +58,6 @@ class Board extends React.Component {
     );
   }
 }
-
 class Game extends React.Component {
   render() {
     return (
